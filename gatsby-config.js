90535f6521bf3f5724ec13f60ae15e4590be7ded
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-10 18:44:44 -0600

    Initial Amanzi commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,31 +2,61 @@ require(`dotenv`).config()
 
 module.exports = {
   siteMetadata: {
-    title: `Flex Starter`,
-    description: `Starter for the Flex theme.`,
+    title: `Amanzi/ATS`,
+    description: `Landing page for Amanzi/ATS`,
     siteUrl: process.env.SITE_URL,
     logo: `logo.png`,
-    copyright: `© YYYY Flex. All rights reserved.`,
+    copyright: `© 2020 Los Alamos National Laboratory. All rights reserved.`,
     headerLinks: [
       {
         title: `Home`,
         path: `/`,
       },
       {
-        title: `About`,
-        path: `/about`,
+        title: `Case Studies`,
+        path: `/case-studies`,
+      },
+      {
+        title: `Gallery`,
+        path: `/gallery`,
+      },
+      {
+        title: `References`,
+        path: `/references`,
+      },
+      {
+        title: `GitHub`,
+        path: `https://github.com/amanzi`,
       },
     ],
     footerLinks: [
       {
-        title: `Home`,
-        path: `/`,
-      },
-      {
-        title: `About`,
-        path: `/about`,
+        title: `GitHub`,
+        path: `https://github.com/amanzi`,
       },
     ],
   },
-  plugins: [`gatsby-theme-flex`],
+  plugins: [
+    `gatsby-theme-flex`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/assets/images`,
+      },
+    },
+  ],
 }
--- src/gatsby-theme-flex/theme.js
+++ src/gatsby-theme-flex/theme.js
@@ -2,6 +2,19 @@
 // See https://theme-ui.com/theme-spec.
 export default {
   colors: {
-    primary: `#ff6b6b`,
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#05a',
+    muted: '#f6f6f6f',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: '#0cf',
+        secondary: '#09c',
+        muted: '#111',
+      },
+    },
   },
 }
