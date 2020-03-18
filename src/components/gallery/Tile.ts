f9b6096c1d07302e7f23e4dac6ca1baf61a95d61
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-18 02:12:51 -0600

    Some cleanup

--- src/components/gallery/Tile.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-const Tile = styled.div`
-  position: relative;
-  cursor: pointer;
-  overflow: hidden;
-`;
-
-export default Tile;
--- src/gatsby-theme-flex/blocks.js
+++ src/gatsby-theme-flex/blocks.js
@@ -1,8 +1,5 @@
 // src/gatsby-theme-flex/blocks.js
 export * from "gatsby-theme-flex/src/blocks"
-export { default as MyComponent } from "../my-component.js"
-export { default as Video } from "../video.js"
 export { default as PhotoGallery } from "../components/image-gallery/Gallery.js"
 export { default as VideoGallery } from "../components/video-gallery/Gallery.js"
 export { default as BibTex } from "../components/bibtex/BibTex.js"
-export { default as Gallery } from "../components/gallery/Gallery"
