9f4cc1dc4da866dc88303773d645695628225d4c
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-17 22:00:31 -0600

    Moved photo gallery into dedicated components dir

--- src/gatsby-theme-flex/blocks.js
+++ src/gatsby-theme-flex/blocks.js
@@ -1,7 +1,7 @@
 // src/gatsby-theme-flex/blocks.js
 export * from "gatsby-theme-flex/src/blocks"
 export { default as MyComponent } from "../my-component.js"
-export { default as TestGall } from "../testgall.js"
+export { default as PhotoGallery } from "../components/image-gallery/Gallery.js"
 export { default as Video } from "../video.js"
 export { default as BibTex } from "../bibtex.js"
 export { default as Gallery } from "../components/gallery/Gallery"
