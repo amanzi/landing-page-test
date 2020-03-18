0bdf34fe7efe19868662e425ca858bc4fa9c428b
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-17 23:04:11 -0600

    Finalized photo gallery

--- src/components/image-gallery/Gallery.js
+++ src/components/image-gallery/Gallery.js
@@ -3,6 +3,8 @@ import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
 import "lightgallery.js/dist/css/lightgallery.min.css";
 import "./styles.css";
 
+let groupID = 0;
+
 const PhotoItem = ({ image, group, caption }) => (
   <div className="justified-gallery">
     <LightgalleryItem group={group} src={image} subHtml={"<h3>"+caption+"</h3>"} style={{width: "192px", height: "129.8802681992337px", top: "6px", left: "6px", opacity: "1" }}>
@@ -12,6 +14,10 @@ const PhotoItem = ({ image, group, caption }) => (
 );
 
 const PhotoGallery = ({heading, photos}) => {
+
+  groupID++;
+  const groupStr = "group" + groupID;
+
   return (
     <LightgalleryProvider
       onAfterSlide={() => {
@@ -25,10 +31,12 @@ const PhotoGallery = ({heading, photos}) => {
       plugins={["lg-thumbnail.js"]}
     >
       <h1 style={{ }}>{heading}</h1>
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
       {
         Object.keys(photos).map((p, idx) => ( 
-          <PhotoItem key={idx} image={p} group="group2" caption={photos[p]} />
+          <div>
+            <PhotoItem key={idx} image={p} group={groupStr} caption={photos[p]} />
+          </div>
         ))
       }
       </div>
