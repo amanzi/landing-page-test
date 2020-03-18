98bbe31c66c007f76770cc783fa4fd322df69a19
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-17 22:37:32 -0600

    Update to photo gallery component

--- src/components/image-gallery/Gallery.js
+++ src/components/image-gallery/Gallery.js
@@ -1,30 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
-//
 import "lightgallery.js/dist/css/lightgallery.min.css";
 import "./styles.css";
 
-const GROUP1 = [
-  "https://images.unsplash.com/flagged/photo-1551706646-9c816bfbff8c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
-  "https://images.unsplash.com/photo-1551633550-64761da5342b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
-];
-
-const GROUP2 = [
-  "https://images.unsplash.com/photo-1551833726-deb5e781c68f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-  "https://images.unsplash.com/photo-1551803021-92431219e83e?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-  "https://images.unsplash.com/photo-1551852284-ce16dd4d63d3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-];
-
-const PhotoItem = ({ image, group }) => (
+const PhotoItem = ({ image, group, caption }) => (
   <div className="justified-gallery">
-    <LightgalleryItem group={group} src={image} subHtml={".caption"} style={{width: "192px", height: "129.8802681992337px", top: "6px", left: "6px", opacity: "1" }}>
-        <img className="gallery-img" src={image} style={{ width: "199px", height: "135px", cursor: "pointer" }} />
+    <LightgalleryItem group={group} src={image} subHtml={"<h3>"+caption+"</h3>"} style={{width: "192px", height: "129.8802681992337px", top: "6px", left: "6px", opacity: "1" }}>
+        <img className="gallery-img" src={image} alt="myimage" style={{ width: "199px", height: "135px", cursor: "pointer" }} />
     </LightgalleryItem>
   </div>
 );
 
-const Test = () => {
+const PhotoGallery = ({heading, photos}) => {
   return (
     <LightgalleryProvider
       onAfterSlide={() => {
@@ -37,20 +24,16 @@ const Test = () => {
       }}
       plugins={["lg-thumbnail.js"]}
     >
-      <h1 style={{ textAlign: "center" }}>Group 1</h1>
+      <h1 style={{ }}>{heading}</h1>
       <div style={{ display: "flex", alignItems: "center" }}>
-        {GROUP1.map((p, idx) => (
-          <PhotoItem key={idx} image={p} group="group1" />
-        ))}
-      </div>
-      <h1 style={{ textAlign: "center" }}>Group 2</h1>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        {GROUP2.map((p, idx) => (
-          <PhotoItem key={idx} image={p} group="group2" />
-        ))}
+      {
+        Object.keys(photos).map((p, idx) => ( 
+          <PhotoItem key={idx} image={p} group="group2" caption={photos[p]} />
+        ))
+      }
       </div>
     </LightgalleryProvider>
   );
 }
 
-export default Test;
+export default PhotoGallery;
