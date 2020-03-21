9332ad3ce254b200eae008a80f5b87453f71e5a4
Daniel Reece Livingston <livingston@lanl.gov>
2020-03-20 18:15:12 -0600

    Updated video gallery

--- src/components/video-gallery/Gallery.js
+++ src/components/video-gallery/Gallery.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import ImageGallery from 'react-image-gallery';
 import { Player } from 'video-react';
-import { Carousel } from 'react-responsive-carousel';
+import "react-image-gallery/styles/css/image-gallery.css";
 import "video-react/dist/video-react.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./styles.css";
 
-const VideoGallery = ({videos}) => {
-  return (
-    <div style={{ width: "100%", minWidth: "500px" }}>
-      <Carousel dynamicHeight showThumbs={false} showIndicators={false}>
-        {
-          Object.keys(videos).map((key, index) => (
-            <div>
-              <Player
-                poster={videos[key][1]}
-                src={key}
-              />
-              <p className="legend">
-                {videos[key][0]}
-              </p>
-            </div>
-          ))
-        }
-      </Carousel>
-    </div>
-  )
+class VideoGallery extends React.Component {
+
+    constructor(props) {
+      super(props);
+
+      this.videos = this.props.videos
+
+      for (var i = 0; i < this.videos.length; i++) {
+        this.videos[i].original = this.videos[i].thumbnail;
+        this.videos[i].renderItem = this._renderVideo.bind(this);
+      }
+
+    }
+
+  _toggleShowVideo(url) {
+    this.state.showVideo[url] = !Boolean(this.state.showVideo[url]);
+    this.setState({
+      showVideo: this.state.showVideo
+    });
+  }
+
+  _renderVideo(item) {
+    return (
+      <div>
+        <div className='video-wrapper' style={{ width: "500px" }}>
+            <a
+              className='close-video'
+              onClick={this._toggleShowVideo.bind(this, item.embedUrl)}
+            >
+            </a>
+            <Player
+              poster={item.thumbnail}
+              src={item.embedUrl}
+            />
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return <ImageGallery infinite={false} showPlayButton={false} showFullscreenButton={false} items={this.videos} />;
+  }
 }
 
 export default VideoGallery;
